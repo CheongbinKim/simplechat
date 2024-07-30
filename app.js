6ba4f7c23f56e32d1b892f42b1d4a7a5f8f8ec5f
kev1n <cheongbinkim@gmail.com>
2024-07-30 23:09:32 +0900

    .

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const session = require('express-session');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,6 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+
+// 세션 설정
+app.use(session({
+  secret: 'your_secret_key', // 세션 암호화를 위한 비밀 키
+  resave: false,             // 세션이 수정되지 않아도 세션을 다시 저장할지 여부
+  saveUninitialized: true,   // 초기화되지 않은 세션을 저장할지 여부
+  cookie: { secure: false }  // HTTPS가 아닌 경우 false로 설정
+}));
 
 app.use("/", indexRouter);
 
@@ -41,7 +51,13 @@ app.post("/login", (req, res) => {
 
 // Serve the chat page
 app.get("/chat", (req, res) => {
-  res.render("index");
+  console.log(req.query);
+  if(req.query.k){
+    return res.render("index",{super:true});
+  }else{
+    return res.render("index",{super:false});
+  }
+  
 });
 
 app.use("/users", usersRouter);
